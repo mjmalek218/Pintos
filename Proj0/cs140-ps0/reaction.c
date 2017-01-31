276b32eead24178a266301813ca56efdf9a69824
mjmalek218 <mjmalek218@gmail.com>
2017-01-31 11:54:26 -0600

    Finally finished Proj0. Both caltrain and reaction seem to be functioning.
    The issue in caltrain was the atomicity of the decrementing of the number
    of H atoms prior to signaling, which was fixed by decrementing the number
    in the O function instead of the H one.

--- Proj0/cs140-ps0/reaction.c
+++ Proj0/cs140-ps0/reaction.c
@@ -1,127 +1,91 @@
 #include "pintos_thread.h"
-#include <stdio.h>
-
-/* 
-
-   OVERALL BULLET POINTS IN THE ASSIGNMENT
-
-   --the code must invoke "make_water" exactly once for ever two H
-     and one O atoms that call reaction_h and reaction o, and *only*
-     when these calls are active
-
-   --only one lock allowed
-
-   --no busy waiting
-
-*/
-
-
 
 // Forward declaration. This function is implemented in reaction-runner.c,
 // but you needn't care what it does. Just be sure it's called when
 // appropriate within reaction_o()/reaction_h().
 void make_water();
 
-struct reaction {
+/* Again we attempt to frame our code in terms of the producer/consumer
+   paradigm. The problem here is that, from the outset, it seems as though
+   all the threads are simultaneously producers/consumers, as hydrogen
+   and oxygen are producing/consuming themselves to create water. 
 
-  unsigned int num_H;
-  unsigned int num_O;
+   Our approach will be to frame the reaction in a way un-natural to the
+   chemistry, but natural to our producer/consumer paradigm. The oxygen
+   threads will be the "consumer" threads and the *only* threads
+   calling "make water", and the hydrogen's will be the "producer" 
+   threads, producing themselves, ofc. There will be only one count,
+   tracking the number of hydrogen atoms.
 
-  struct lock reaction_lock;
+   Whenever an H atom is created, it will increment the total number
+   of these atoms and broadcast to oxygen atoms to check for satisfactory
+   conditions (for the reaction to occur: mianly that there are at least
+   2 H atoms). It will then sleep until it is signaled by an oxygen atom 
+   that it is ready for the reaction to occur 
 
-  /* conditions for extra H and O that are waiting due to an imbalanced surplus */
-  struct condition H_wait;
-  struct condition O_wait;
+   Whenver an 
+ 
+*/
+
+
+struct reaction 
+{
+  int num_h;
+
+  struct condition waiting_h, waiting_o; 
+
+  /* all purpose lock. problem specifies we can have at most one */
+  struct lock lock;
 
 };
 
-
 void reaction_init(struct reaction *reaction)
 {
-  reaction->num_H = 0;
-  reaction->num_O = 0;
-
-  lock_init(&reaction->reaction_lock);
-
-  cond_init(&reaction->H_wait);
-  cond_init(&reaction->O_wait);
-
+  reaction->num_h = 0;
+  lock_init(&reaction->lock);
+  cond_init(&reaction->waiting_h);
+  cond_init(&reaction->waiting_o);
 }
 
-/* Invoked by an H atom when it is ready to react */
 void reaction_h(struct reaction *reaction)
 {
-  lock_acquire(&reaction->reaction_lock);
+  lock_acquire(&reaction->lock);
 
-  /* increment *immediately* so that we can assess the current state of the reactions
-     with updated information */
-  reaction->num_H++;
+  reaction->num_h++;
 
-  //DELETE THIS DELETE THIS
-  printf("REACHED H: 1 \n");  fflush(stdout);
+  cond_broadcast(&reaction->waiting_o, &reaction->lock);
 
-  /* If there are enough H, then wake up the O thread and only allow it to signal
-     *once* this hydrogen element is waiting by waiting until cond_wait to release the 
-     lock*/
-  if (reaction->num_H >= 2 && reaction->num_O >= 1)
-    {
-      cond_signal(&reaction->O_wait, &reaction->reaction_lock);
-    }
+  cond_wait(&reaction->waiting_h, &reaction->lock);
 
- 
-  /* wait for the signal from oxygen */
-  cond_wait(&reaction->H_wait, &reaction->reaction_lock);
-
-  //DELETE THIS DELETE THIS
-  printf("REACHED H: 2 \n");  fflush(stdout);
-
-  /* lock has been re-acquired */
-  reaction->num_H--;
-  lock_release(&reaction->reaction_lock);
+  lock_release(&reaction->lock);
 
   return;
 }
 
-/* PROBLEM PROBLEM PROBLEM: THIS CODE ASSUMES THAT HYDROGEN HAPPENS *BEFORE* OXYGEN
-
-   FIXFIXFIX
-
- */
-
-
 void reaction_o(struct reaction *reaction)
 {
-  lock_acquire(&reaction->reaction_lock);
+  lock_acquire(&reaction->lock);
 
-  reaction->num_O++;
-
-  // We only reach here if there is positive oxygen count so no need to check
-  // for that in the condition
-  while (reaction->num_H < 2)
+  while (reaction->num_h < 2)
     {
-      //DELETE THIS DELETE THIS
-      printf("REACHED OXYGEN LOOP\n");  fflush(stdout);
-
-      cond_wait(&reaction->O_wait, &reaction->reaction_lock);
+      cond_wait(&reaction->waiting_o, &reaction->lock);
     }
 
-  /* lock has been re-acquired */
-  reaction->num_O--;
+  /* Signal *exactly* twice and decrement here to ensure the operation
+     is atomic */
 
+  reaction->num_h-= 2;
+  cond_signal(&reaction->waiting_h, &reaction->lock);
+  cond_signal(&reaction->waiting_h, &reaction->lock);
 
-  cond_signal(&reaction->H_wait, &reaction->reaction_lock);
-  cond_signal(&reaction->H_wait, &reaction->reaction_lock);
+  /* One problem here is that we *need* to ensure the atomicity of this operation. 
+     Before we started decrementing the number of h inside this function, 
+     another O atom may have bene created in between and signaled more H atoms
+     to wake up then we wuld have wanted. */
 
+  lock_release(&reaction->lock);
 
-  /* Calling "make-water" in this file makes much more sense so we don't get 
-     multiple calls */
-
-  printf("Make Water is about to be called.\n"); fflush(stdout);
   make_water();
-
-  /* release after the call to make sure that the two hydrogen atoms return 
-     after the call as well. */
-  lock_release(&reaction->reaction_lock);
 
   return;
 
